038c6a6afd97a692ed202f4c6eb188d44ed6ad4b
bl4ckmarchh <xvvsti@gmail.com>
2026-01-15 01:26:24 +0700

    add csrf

--- superlists/settings.py
+++ superlists/settings.py
@@ -140,3 +140,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-4a6b1.up.railway.app',
+]
